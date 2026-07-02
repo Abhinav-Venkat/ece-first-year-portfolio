6ebe45cfc559e61d374f6fe4f52a9fbc6c67eef1
Abhinav Venkat <abhinav.vm07@gmail.com>
2026-07-02 14:32:14 +0400

    My very first GitHub commit

---
+++ hello-ece.py
@@ -0,0 +1,2 @@
+print("Hello! My name is Abhinav, and I'm an ECE student at PSG College of Technology.")
+print("Welcome to my git repository, a collection of small programs I made in my 1st year.")
